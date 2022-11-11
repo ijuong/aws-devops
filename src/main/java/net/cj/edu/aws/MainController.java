5b31f3a9207b2133c96da8d0ed140e61a83f262a
ijuyong <juyong.lee@cj.net>
2022-11-11 14:32:38 +0900

    메인페이지 생성

---
+++ src/main/java/net/cj/edu/aws/MainController.java
@@ -0,0 +1,16 @@
+package net.cj.edu.aws;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+
+	@GetMapping("/")
+	public String home() {
+		return "<h1>aws-devops</h1>";
+	}
+}
